a902342dcda52ea3d9eee1013e37eebf0a83ab89
suspiciouscap <dan.hong378@gmail.com>
2025-05-29 21:36:22 +1000

    feat: POC

--- renderer/server.js
+++ renderer/server.js
@@ -2,17 +2,39 @@ import chokidar from 'chokidar';
 import express from 'express';
 import http from 'http';
 import { WebSocketServer } from 'ws';
+import WebSocket from 'ws';
+import { exec } from 'child_process';
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-const watcher = chokidar.watch('../mermaid_files', { 
-  ignored: (path, stats) => stats?.isFile() && !path.endsWith('.mmd'),
+wss.on('connection', (ws) => {
+  sendFile(ws, '../mermaid_files/edit.mmd');
 });
 
-watcher.on('change', (path )=> {});
+const watcher = chokidar.watch('../mermaid_files/edit.mmd', {
+  ignoreInitial: false
+});
 
-server.listen(8080, () => {
-  console.log('Server and WebSocket running on port 8080');
+function sendFile(ws, filePath) {
+  exec(`mmdc -i ${filePath} --output -`, { maxBuffer: 10 * 1024 * 1024 }, (error, stdout) => {
+    if (error) return;
+    const message = JSON.stringify({ content: stdout });
+    ws.send(message);
+  });
+}
+
+watcher.on(
+  'change', (path) => {
+    for (const ws of wss.clients) {
+      if (ws.readyState === WebSocket.OPEN) {
+        sendFile(ws, path);
+      }
+    }
+  }
+);
+
+server.listen(2195, () => {
+  console.log('Server and WebSocket running on port 2195');
 });
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './components/ui/resizable'
+import { webSocket } from 'rxjs/webSocket'
+import { Subject } from 'rxjs';
+
+interface SvgFile {
+  content: String
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+
+
+  const [svg, setSvg] = useState<String>("")
+
+  useEffect(() => {
+    const socket$ = webSocket<SvgFile>("ws://localhost:2195");
+    const subscription = socket$.subscribe({
+
+      next: (msg: SvgFile) => {
+
+
+        setSvg(msg.content)
+      },
+      error: () => { },
+      complete: () => { }
+    });
+    return () => subscription.unsubscribe();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ResizablePanelGroup direction='horizontal'>
+        <ResizablePanel>
+          Folder picker - to be implemented
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel>
+          <div dangerouslySetInnerHTML={{ __html: svg }} />
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </>
   )
 }
